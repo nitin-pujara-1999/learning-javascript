ff03fce52043296b8cfe3be71cfeb7e45daee3dc
nitin-pujara-1999 <120445390+nitin-pujara-1999@users.noreply.github.com>
2022-12-30 17:52:53 +0000

    push value in an array

--- 01_bascis/test.js
+++ 01_bascis/test.js
@@ -4,8 +4,16 @@
 // console.log(a)
 // console.log(b)
 
-var obj1 = {Name: "Nitin", Age: '23'}
-var obj2 = obj1;
+// var obj1 = {Name: "Nitin", Age: '23'}
+// var obj2 = obj1;
 
-console.log(obj1)
-console.log(obj2)
+// obj2.Age = '22';
+
+// console.log(obj1)
+// console.log(obj2)
+
+var a = [1,2,3,4,5];
+var b = [].concat(a);
+b.push(8446);
+console.log(a)
+console.log(b)
